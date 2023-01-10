8601e4ed65760814702f626e62ef6bc63b265bae
binisnull <dbsqlssss0704@gmail.com>
2023-01-11 01:09:17 +0900

    feat: 파일 구성 변경 및 GradeTableView 완성

--- FITI-Trainer-iOS/FITI-Trainer-iOS/GradeTableViewController.swift
+++ FITI-Trainer-iOS/FITI-Trainer-iOS/GradeTableViewController.swift
@@ -10,12 +10,33 @@ import SnapKit
 
 class GradeTableViewController: UIViewController {
     
-    var titleLabel : UILabel = {
+    private let gradeImage : UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(named:"gradeTable.svg")
+            return imageView
+        }()
+    
+    private let announceFirstLabel : UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 25)
-        label.text = "Grade Table View"
+        label.text = "*자격증 업데이트를 통해 언제든지 Level-UP 할 수 있습니다. "
+        label.font = UIFont.systemFont(ofSize: 12)
         label.textColor = UIColor.customColor(.blue)
         return label
+    }()
+    
+    private let announceSecLabel : UILabel = {
+        let label = UILabel()
+        label.text = "My page내의 자격증 업데이트 칸을 이용해주세요."
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.customColor(.blue)
+        return label
+    }()
+    
+    private let announceStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        return stackView
     }()
 
     override func viewDidLoad() {
@@ -25,27 +46,50 @@ class GradeTableViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .black
         navigationController?.navigationBar.topItem?.title = ""
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image:UIImage(named: "leftIcon.svg"), style: .plain, target: self, action: #selector(backTapped))
-        
+        pagingTimer()
         // Do any additional setup after loading the view.
         setViewHierarchy()
         setConstraints()
     }
 
     private func setViewHierarchy() {
-        view.addSubview(titleLabel)
+        view.addSubview(gradeImage)
+        announceStackView.addArrangedSubview(announceFirstLabel)
+        announceStackView.addArrangedSubview(announceSecLabel)
+
+        view.addSubview(announceStackView)
     }
     
     private func setConstraints(){
         
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(70)
-            make.centerX.equalToSuperview()
-            
+        gradeImage.snp.makeConstraints { make in
+            make.height.equalTo(395)
+            make.width.equalTo(349)
+            make.top.equalToSuperview().offset(147)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        announceStackView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-90)
+            make.leading.equalToSuperview().offset(43)
+            make.trailing.equalToSuperview().offset(-42)
         }
     }
     
     @objc func backTapped(sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    func pagingTimer() {
+            let _: Timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { (Timer) in
+                self.pagingMove()
+            }
+        }
+    
+    func pagingMove() {
+        let nextVC = TabBarController()
+        navigationController?.pushViewController(nextVC, animated: true)
     }
 
 }
